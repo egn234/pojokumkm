a714e7c5bd79a8276720dad6d0b2bf041a685d70
Egan Kusmaya Putra <egn234@gmail.com>
2022-05-27 15:10:36 +0700

    CRUD kelola iklan di pengelola updated

--- app/Controllers/pengelola/Iklan.php
+++ app/Controllers/pengelola/Iklan.php
@@ -4,6 +4,7 @@
 	use App\Controllers\BaseController;
 	use App\Models\M_pengelola;
 	use App\Models\M_user;
+	use App\Models\M_ads;
 	use CodeIgniter\Files\File;
 
 	class Iklan extends \App\Controllers\BaseController{
@@ -11,6 +12,7 @@
 		function __construct(){
 			$this->m_pengelola = new M_pengelola();
 			$this->m_user = new M_user();
+			$this->m_ads = new M_ads();
 			$this->request = \Config\Services::request();
 		}
 
@@ -25,7 +27,7 @@
 		}
 
 		public function index(){
-			return redirect()->to(base_url('pengelola/produk/list'));
+			return redirect()->to(base_url('pengelola/iklan/list'));
 		}
 
 		public function list(){
@@ -33,7 +35,7 @@
 	    $iduser = session()->get('iduser');
 			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
 
-			$l_iklan = $this->m_produk->getAllProduk();
+			$l_iklan = $this->m_ads->getAllAds();
 			$detilUser = $this->m_pengelola->getJoinUserPengelola($iduser)[0];
 
 			$data = [
@@ -42,6 +44,187 @@
 				'detail_user' => $detilUser
 			];
 			
-			return view('pengelola/prod/list-produk', $data);
+			return view('pengelola/ads/list-iklan', $data);
+		}
+
+		public function detail($idads){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$l_detail = $this->m_ads->getAdsById($idads)[0];
+			$detilUser = $this->m_pengelola->getJoinUserPengelola($iduser)[0];
+
+			$data = [
+				'title_meta' => view('partials/title-meta', ['title' => 'List Paket Iklan']),
+				'l_detail' => $l_detail,
+				'detail_user' => $detilUser
+			];
+			
+			return view('pengelola/ads/detail-iklan', $data);
+		}
+
+		public function add(){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$detilUser = $this->m_pengelola->getJoinUserPengelola($iduser)[0];
+
+			$data = [
+				'title_meta' => view('partials/title-meta', ['title' => 'Tambah Paket Iklan']),
+				'detail_user' => $detilUser
+			];
+			
+			return view('pengelola/ads/add-iklan', $data);
+		}
+
+		public function edit($idads){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$l_detail = $this->m_ads->getAdsById($idads)[0];
+			$detilUser = $this->m_pengelola->getJoinUserPengelola($iduser)[0];
+
+			$data = [
+				'title_meta' => view('partials/title-meta', ['title' => 'Tambah Paket Iklan']),
+				'l_detail' => $l_detail,
+				'detail_user' => $detilUser
+			];
+			
+			return view('pengelola/ads/edit-iklan', $data);
+		}
+
+		public function add_proc(){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$ads_name = $_POST['ads_name'];
+
+			$ceknama = $this->m_ads->countAdsByName($ads_name)[0]->hitung;
+			
+			if ($ceknama > 0) {
+				$alert = view('partials/notification-alert', 
+					['notif_text' => 'paket sudah terdaftar pada aplikasi',
+					 'status' => 'warning']
+					);
+				session()->setFlashdata('notif', $alert);
+				return redirect()->to(base_url('pengelola/iklan/add'));
+			}
+
+			if ($_POST['duration1'] < 1) {
+				$alert = view('partials/notification-alert', 
+					['notif_text' => 'durasi tidak boleh kurang dari 1',
+					 'status' => 'warning']
+					);
+				session()->setFlashdata('notif', $alert);
+				return redirect()->to(base_url('pengelola/iklan/add'));
+			}
+
+			$ads_description = $_POST['ads_description'];
+			$ads_duration = $_POST['duration'];
+			$ads_price = $_POST['ads_price'];
+
+			$dataset = [
+				'ads_name' => $ads_name,
+				'ads_duration' => $ads_duration,
+				'ads_description' => $ads_description,
+				'ads_price' => $ads_price,
+				'ads_status' => 'on'
+			];
+
+			$this->m_ads->insertAds($dataset);
+
+			$alert = view('partials/notification-alert', 
+				['notif_text' => 'Paket Iklan berhasil ditambahkan',
+				 'status' => 'success']
+				);
+
+			session()->setFlashdata('notif', $alert);
+			return redirect()->to(base_url('pengelola/iklan/list'));
+		}
+
+		public function edit_proc($idads){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$temp_data = $this->m_ads->getAdsById($idads)[0];
+
+			$ads_name = $_POST['ads_name'];
+
+			if ($ads_name != $temp_data->ads_name) {
+				$ceknama = $this->m_ads->countAdsByName($ads_name)[0]->hitung;
+				
+				if ($ceknama > 0) {
+					$alert = view('partials/notification-alert', 
+						['notif_text' => 'paket sudah terdaftar pada aplikasi',
+						 'status' => 'warning']
+						);
+					session()->setFlashdata('notif', $alert);
+					return redirect()->to(base_url('pengelola/iklan/add'));
+				}
+			}
+
+			if ($_POST['duration1'] < 1) {
+				$alert = view('partials/notification-alert', 
+					['notif_text' => 'durasi tidak boleh kurang dari 1',
+					 'status' => 'warning']
+					);
+				session()->setFlashdata('notif', $alert);
+				return redirect()->to(base_url('pengelola/iklan/add'));
+			}
+
+			$ads_description = $_POST['ads_description'];
+			$ads_duration = $_POST['duration1'];
+			$ads_price = $_POST['ads_price'];
+
+			$dataset = [
+				'ads_name' => $ads_name,
+				'ads_duration' => $ads_duration,
+				'ads_description' => $ads_description,
+				'ads_price' => $ads_price,
+			];
+
+			$this->m_ads->updateAds($dataset, $idads);
+
+			$alert = view('partials/notification-alert', 
+				['notif_text' => 'Paket Iklan berhasil diubah',
+				 'status' => 'success']
+				);
+
+			session()->setFlashdata('notif', $alert);
+			return redirect()->to(base_url('pengelola/iklan/detail/'.$idads));
+		}
+
+		public function switch($idads){
+			$this->newUser();
+	    $iduser = session()->get('iduser');
+			$idpengelola = $this->m_pengelola->getJoinUserPengelola($iduser)[0]->idpengelola;
+
+			$status = $this->m_ads->getAdsById($idads)[0]->ads_status;
+			
+			if ($status == 'on') {
+				$ads_status = 'off';
+				$alert = view('partials/notification-alert', 
+					['notif_text' => 'Paket Iklan dinonaktifkan',
+					 'status' => 'success']
+					);
+
+				session()->setFlashdata('notif', $alert);
+			}else{
+				$ads_status = 'on';
+				$alert = view('partials/notification-alert', 
+					['notif_text' => 'Paket Iklan diaktifkan',
+					 'status' => 'success']
+					);
+
+				session()->setFlashdata('notif', $alert);
+			}
+
+			$this->m_ads->switchAdsStatus($ads_status, $idads);
+			return redirect()->to(base_url('pengelola/iklan/detail/'.$idads));
 		}
 	}
