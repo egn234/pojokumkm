6a76371870f04887712be6ea0910f7cacbf59ebd
rianraka <rianraka22@gmail.com>
2022-06-11 12:07:40 +0700

    Recom Ads Tambahan dari om

--- app/Controllers/Produk.php
+++ app/Controllers/Produk.php
@@ -21,14 +21,20 @@ class Produk extends BaseController
 		$kategori = (isset($_GET['kategori'])) ?  $_GET['kategori'] : [];
 
 		if (isset($_GET['search']) || isset($_GET['kategori'])) {
+			$keyword = ["category_name LIKE '%$kategori[0]%'"];
+			//LOOP BUAT CARI KATEGORI
+			for ($i = 1; $i < count($kategori); $i++) {
+				array_push($keyword, "OR category_name LIKE '%$kategori[$i]%' ");
+			}
+
 			if ($_GET['search'] == "" && isset($_GET['kategori'])) {
 				$keyword = ["category_name LIKE '%$kategori[0]%'"];
 
 				//LOOP BUAT CARI KATEGORI
 				for ($i = 1; $i < count($kategori); $i++) {
 					array_push($keyword, "OR category_name LIKE '%$kategori[$i]%' ");
-				}	
-			}else{
+				}
+			} else {
 				$keyword = ["product_name LIKE '%$search[0]%'"];
 				//LOOP BUAT CARI NAMA PRODUK
 				for ($i = 1; $i < count($search); $i++) {
@@ -41,6 +47,8 @@ class Produk extends BaseController
 			}
 
 			$query = implode(" ", $keyword);
+			$getProdukAdsRandom = $this->m_produk->getProdukAdsRandom($query);
+
 			$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
 
 			// Jumlah data per halaman
@@ -52,7 +60,7 @@ class Produk extends BaseController
 		} else {
 			$allProduk = count($this->m_produk->getAllproduk());
 			$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
-
+			$getProdukAdsRandom = $this->m_produk->getProdukAdsRandom();
 			// Jumlah data per halaman
 			$limit = 12;
 			$jumlahPage = ceil($allProduk / $limit);
@@ -61,7 +69,6 @@ class Produk extends BaseController
 		}
 		$getAllkategori = $this->m_kategori->getAllKategori();
 
-		$getProdukAdsRandom = $this->m_produk->getProdukAdsRandom();
 		$data = [
 			'title_meta' => view('homepage_partial/title-meta', ['title' => 'List Produk']),
 			'l_produk' => $l_produk,
@@ -89,7 +96,60 @@ class Produk extends BaseController
 		return view('detail-produk', $data);
 	}
 
-	public function gridDisplay()
+	public function recom()
 	{
+		$search = (isset($_GET['search'])) ? explode(" ", $_GET['search']) : "";
+		$kategori = (isset($_GET['kategori'])) ?  $_GET['kategori'] : [];
+
+		if (isset($_GET['search']) || isset($_GET['kategori'])) {
+			if ($_GET['search'] == "" && isset($_GET['kategori'])) {
+				$keyword = ["category_name LIKE '%$kategori[0]%'"];
+
+				//LOOP BUAT CARI KATEGORI
+				for ($i = 1; $i < count($kategori); $i++) {
+					array_push($keyword, "OR category_name LIKE '%$kategori[$i]%' ");
+				}
+			} else {
+				$keyword = ["product_name LIKE '%$search[0]%'"];
+				//LOOP BUAT CARI NAMA PRODUK
+				for ($i = 1; $i < count($search); $i++) {
+					array_push($keyword, "OR product_name LIKE '%$search[$i]%' ");
+				}
+				//LOOP BUAT CARI KATEGORI
+				for ($i = 0; $i < count($kategori); $i++) {
+					array_push($keyword, "OR category_name LIKE '%$kategori[$i]%' ");
+				}
+			}
+
+			$query = implode(" ", $keyword);
+
+			$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
+
+			// Jumlah data per halaman
+			$limit = 12;
+			$limitStart = ($page - 1) * $limit;
+			$allProduk = count($this->m_produk->getAllAdsProduk());
+			$jumlahPage = ceil($allProduk / $limit);
+			$l_produk = $this->m_produk->getAllAdsProduk($limit, $limitStart, $search, $query);
+		} else {
+			$allProduk = count($this->m_produk->getAllAdsProduk());
+			$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
+
+			// Jumlah data per halaman
+			$limit = 12;
+			$jumlahPage = ceil($allProduk / $limit);
+			$limitStart = ($page - 1) * $limit;
+			$l_produk = $this->m_produk->getAllAdsProduk($limit, $limitStart);
+		}
+		$getAllkategori = $this->m_kategori->getAllKategori();
+
+		$data = [
+			'title_meta' => view('homepage_partial/title-meta', ['title' => 'List Produk']),
+			'l_produk' => $l_produk,
+			'l_page' => $page,
+			'jumlahPage' => $jumlahPage,
+			'kategori' => $getAllkategori
+		];
+		return view('recom-page', $data);
 	}
 }
